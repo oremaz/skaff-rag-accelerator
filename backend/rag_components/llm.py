c917f055548ee095c30a5363ca8ee24dd6a8694d
Orel MAZOR <orel.mazor@FRATF-C02H88C7Q6L4-M.local>
2025-06-06 16:24:34 +0200

    Minor updates2

--- backend/rag_components/llm.py
+++ backend/rag_components/llm.py
@@ -7,16 +7,22 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from backend.config import RagConfig
 
 
+import inspect
+
 def get_llm_model(config: RagConfig, callbacks: List[BaseCallbackHandler] = []):
     if config.llm.source == "ChatGoogleGenerativeAI":
         llm_spec = ChatGoogleGenerativeAI
     else:
         from langchain_community import chat_models
         llm_spec = getattr(chat_models, config.llm.source)
+
+    # Obtenir la liste des paramètres du constructeur
+    valid_params = inspect.signature(llm_spec).parameters
+
     kwargs = {
         key: value
         for key, value in config.llm.source_config.items()
-        if key in llm_spec.model_fields.keys()
+        if key in valid_params
     }
     kwargs["streaming"] = True
     kwargs["callbacks"] = callbacks
