9a683f9adbf9d549b7b7d06265d22b1c00cce3f5
Orel MAZOR <orel.mazor@FRATF-C02H88C7Q6L4-M.local>
2025-06-06 15:15:04 +0200

    “Minor_updates”

--- backend/rag_components/llm.py
+++ backend/rag_components/llm.py
@@ -2,16 +2,21 @@ from typing import List
 
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain_community import chat_models
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 from backend.config import RagConfig
 
 
 def get_llm_model(config: RagConfig, callbacks: List[BaseCallbackHandler] = []):
-    llm_spec = getattr(chat_models, config.llm.source)
+    if config.llm.source == "ChatGoogleGenerativeAI":
+        llm_spec = ChatGoogleGenerativeAI
+    else:
+        from langchain_community import chat_models
+        llm_spec = getattr(chat_models, config.llm.source)
     kwargs = {
         key: value
         for key, value in config.llm.source_config.items()
-        if key in llm_spec.__fields__.keys()
+        if key in llm_spec.model_fields.keys()
     }
     kwargs["streaming"] = True
     kwargs["callbacks"] = callbacks
